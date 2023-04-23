41a0210e4c4a7407f565a076ac73de8401e4bc4c
ShafwanAbd <217006104@student.unsil.ac.id>
2023-04-23 09:53:42 +0700

    ok

--- resources/views/main/pertanyaan.blade.php
+++ resources/views/main/pertanyaan.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.sidebar')
 
 @section('content') 
+    <div class="container_pertanyaan">
+        <div class="navbar1">
+             <h6>out.png</h6>
+        </div>
+        <div class="navbar2 flex">
+            <h4>Pertanyaan</h4>
+            <a class="btn">Buat Topik Diskusi</a>
+        </div>
+        <div class="container_isi">
+            <div class="isi">
+                <h4>p</h4>
+            </div>
+        </div>
+    </div>
 @endsection
