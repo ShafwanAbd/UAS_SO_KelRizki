cc1fae32911c584fb6dba100bd16c556a2099373
ZulfanAlfarra <zulfansyahidanalfarra.91@gmail.com>
2023-04-25 18:46:16 +0700

    perbaikan landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 <div class="container">
 
-<<<<<<< HEAD
     <div class="row mb-5">
         <div class="col-8">
             <div class="w-75 px-5">        
@@ -20,25 +19,6 @@
         <div class="col-4 d-flex align-items-center">
             <img class="img-fluid " src="{{ asset('./image/img1.jpg')}}" alt="isi foto">
         </div>
-=======
-  <div class="row mb-5">
-    <div class="col-8">
-      <div class="w-75 px-5">
-        <p class="h2 text-capitalize pt-5">investasi bisnis secara bersama-sama dimanapun, kapanpun secara transparan
-          dan aman</p>
-        <p class="my-3">
-          Investasi ke berbagai bisnis berkualitas, mulai dari UMKM, Waralaba, Startup hingga Proyek
-          dengan modal kecil,transparan dan aman
-          secara bersama-sama, karena sudah
-          berizin serta diawasi oleh OJK.
-        </p>
-        <button class="w-100 rounded-pill button1"><a class="text-decoration-none btn" href="{{ route('login') }}">Mulai
-            Investasi</a></button>
-      </div>
->>>>>>> 666b0fe7757e9e49d22a576cf24197af9adb2ff0
-    </div>
-    <div class="col-4 d-flex align-items-center text-center justify-content-center">
-      <img class="border-0 px-3" src="{{ asset('./image/img1.jpg')}}" alt="isi foto" height="300px" width="500px">
     </div>
   </div>
 
@@ -68,18 +48,6 @@
     </div>
   </div>
 </div>
-<<<<<<< HEAD
-<div class="row py-5 mx-0">
-    <div class="col color1 py-5">
-        <div class="w-50 container py-5">
-            <p class="h3">Semakin mudah & aman berinvestasi dalam satu genggaman melalui aplikasi Ternak Connect </p>
-            <p class="my-4">Daftarkan diri anda di aplikasi Ternak Connect sekarang juga dan rasakan kemudahannya!</p>
-            <button class="w-100 rounded-pill button2"><a class="btn" href="{{ route('register') }}">Daftar Sekarang</a></button>
-        </div>    
-    </div>
-    <div class="col color1 d-flex align-items-center">
-        <img class="img-fluid img-thumbnail align-middle" src="{{ asset('./image/img1.jpg')}}" alt="isi foto">
-=======
 <div class="row py-5">
   <div class="col color1 py-5">
     <div class="w-50 container py-5">
@@ -87,7 +55,6 @@
       <p class="my-4">Daftarkan diri anda di aplikasi Ternak Connect sekarang juga dan rasakan kemudahannya!</p>
       <button class="w-100 rounded-pill button2"><a class="btn" href="{{ route('register') }}">Daftar
           Sekarang</a></button>
->>>>>>> 666b0fe7757e9e49d22a576cf24197af9adb2ff0
     </div>
   </div>
   <div class="col color1 d-flex align-items-center">
