02b7268be0ff6aade92d103f755b46629ddbd453
ZulfanAlfarra <zulfansyahidanalfarra.91@gmail.com>
2023-05-02 23:20:39 +0700

    about us

--- resources/views/navbar/about_us.blade.php
+++ resources/views/navbar/about_us.blade.php
@@ -1,5 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="container">
+        <div class="text-center my-5">
+            <p>Tentang Kami</p>
+            <p class="fw-bolder h1 text-dark text-capitalize">Ternak Connect</p>
+        </div>
+        <div class="row  mb-5">
+            <div class="col-4">
+                <img class="img-fluid my-3" src="{{ asset('./image/poto_profil/poto_profil_1.png')}}" alt="">
+            </div>
+            <div class="col-4">
+                <img class="img-fluid my-3" src="{{ asset('./image/poto_profil/poto_profil_1.png')}}" alt="">
+            </div>
+            <div class="col-4">
+                <img class="img-fluid my-3" src="{{ asset('./image/poto_profil/poto_profil_1.png')}}" alt="">
+            </div>
+            <div class="col-4">
+                <img class="img-fluid my-3" src="{{ asset('./image/poto_profil/poto_profil_1.png')}}" alt="">
+            </div>
+            <div class="col-4">
+                <img class="img-fluid my-3" src="{{ asset('./image/poto_profil/poto_profil_1.png')}}" alt="">
+            </div>
+            <div class="col-4">
+                <img class="img-fluid my-3" src="{{ asset('./image/poto_profil/poto_profil_1.png')}}" alt="">
+            </div>
+        </div>
+        <div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus necessitatibus suscipit soluta odio sed ratione consequatur recusandae eveniet ex officia quasi repellat id, harum dolore neque alias amet reprehenderit ad.
+            Minus deserunt doloribus rem consequuntur quod, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum quam, eius molestiae voluptate eaque dolorum nostrum ex explicabo nemo iste neque sapiente quia. Officiis ex natus praesentium. Qui, reprehenderit enim?
+            Excepturi sequi optio minus quaerat, praesentium perferendis! Numquam accusantium et dolorem, adipisci dolorum dolores facilis animi soluta non minima sapiente illum. Cupiditate qui aperiam fugiat ex a doloremque reprehenderit nihil.
+            Sapiente, omnis eius. Corporis nisi ipsa quam error molestias neque odit, repudiandae provident veritatis saepe amet vel cupiditate rerum omnis commodi. Repellat ea neque praesentium accusamus recusandae. Laborum, debitis numquam? corrupti nostrum voluptas voluptatum. Ut alias recusandae, vel quod repellendus sint aliquid dolore voluptatum veniam aspernatur repudiandae, quaerat quo expedita quasi facilis adipisci nulla!</p>
+        </div>
+    </div>
 @endsection
