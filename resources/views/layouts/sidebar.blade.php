b40a5a4a0ef3621ba970b445433ea613b6ca2e95
RizkiPrtma1 <217006106@student.unsil.ac.id>
2023-05-02 22:28:39 +0700

    aktifitas transaksii

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head> 
+<head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- JQuery -->
   <script src="{{ asset('js/jquery-3.6.4.min.js')}}"></script>
